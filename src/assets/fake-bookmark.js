638167dcb0a25b41136bb1a88a8b328434472b00
rezzocrypt <rezzocrypt@gmail.com>
2026-09-17 11:42:07 +0300

    Набор тестовых данных для отладки. Чиню не работавшие тесты

--- src/assets/fake-bookmark.js
+++ src/assets/fake-bookmark.js
@@ -1,5 +1,179 @@
 const fakeBookmark = [
-
+  {
+    "dateAdded": 1458052899259,
+    "id": "0",
+    "title": "",
+    "children": [
+      {
+        "dateAdded": 1458052899259,
+        "dateGroupModified": 1458052899259,
+        "id": "1",
+        "parentId": "0",
+        "title": "",
+        "syncing": true,
+        "children": [
+          {
+            "dateAdded": 1700000000000,
+            "dateGroupModified": 1700000000000,
+            "id": "10",
+            "index": 0,
+            "parentId": "1",
+            "title": "Проекты",
+            "syncing": true,
+            "children": [
+              {
+                "dateAdded": 1700001000000,
+                "dateGroupModified": 1700001000000,
+                "id": "11",
+                "index": 0,
+                "parentId": "10",
+                "title": "Frontend",
+                "syncing": true,
+                "children": [
+                  {
+                    "dateAdded": 1700001010000,
+                    "id": "110",
+                    "index": 0,
+                    "parentId": "11",
+                    "title": "Vue.js",
+                    "url": "https://vuejs.org/"
+                  },
+                  {
+                    "dateAdded": 1700001020000,
+                    "id": "111",
+                    "index": 1,
+                    "parentId": "11",
+                    "title": "Vite",
+                    "url": "https://vite.dev/"
+                  },
+                  {
+                    "dateAdded": 1700001030000,
+                    "id": "112",
+                    "index": 2,
+                    "parentId": "11",
+                    "title": "Vitest",
+                    "url": "https://vitest.dev/"
+                  }
+                ]
+              },
+              {
+                "dateAdded": 1700002000000,
+                "dateGroupModified": 1700002000000,
+                "id": "12",
+                "index": 1,
+                "parentId": "10",
+                "title": "Backend",
+                "syncing": true,
+                "children": [
+                  {
+                    "dateAdded": 1700002010000,
+                    "id": "120",
+                    "index": 0,
+                    "parentId": "12",
+                    "title": "Node.js",
+                    "url": "https://nodejs.org/"
+                  },
+                  {
+                    "dateAdded": 1700002020000,
+                    "id": "121",
+                    "index": 1,
+                    "parentId": "12",
+                    "title": "Express",
+                    "url": "https://expressjs.com/"
+                  }
+                ]
+              },
+              {
+                "dateAdded": 1700003000000,
+                "id": "13",
+                "index": 2,
+                "parentId": "10",
+                "title": "GitHub",
+                "url": "https://github.com/"
+              }
+            ]
+          },
+          {
+            "dateAdded": 1700004000000,
+            "dateGroupModified": 1700004000000,
+            "id": "20",
+            "index": 1,
+            "parentId": "1",
+            "title": "Развлечения",
+            "syncing": true,
+            "children": [
+              {
+                "dateAdded": 1700004010000,
+                "id": "200",
+                "index": 0,
+                "parentId": "20",
+                "title": "YouTube",
+                "url": "https://youtube.com/"
+              },
+              {
+                "dateAdded": 1700004020000,
+                "id": "201",
+                "index": 1,
+                "parentId": "20",
+                "title": "Twitch",
+                "url": "https://www.twitch.tv/"
+              },
+              {
+                "dateAdded": 1700004030000,
+                "id": "202",
+                "index": 2,
+                "parentId": "20",
+                "title": "Spotify",
+                "url": "https://open.spotify.com/"
+              }
+            ]
+          },
+          {
+            "dateAdded": 1700005000000,
+            "dateGroupModified": 1700005000000,
+            "id": "30",
+            "index": 2,
+            "parentId": "1",
+            "title": "Пустая папка",
+            "syncing": true,
+            "children": []
+          },
+          {
+            "dateAdded": 1700006000000,
+            "id": "40",
+            "index": 3,
+            "parentId": "1",
+            "title": "Поиск",
+            "url": "https://www.google.com/"
+          },
+          {
+            "dateAdded": 1700007000000,
+            "id": "41",
+            "index": 4,
+            "parentId": "1",
+            "title": "Википедия",
+            "url": "https://ru.wikipedia.org/"
+          },
+          {
+            "dateAdded": 1700008000000,
+            "id": "42",
+            "index": 5,
+            "parentId": "1",
+            "title": "DuckDuckGo",
+            "url": "https://duckduckgo.com/"
+          },
+          {
+            "dateAdded": 1700009000000,
+            "id": "43",
+            "index": 6,
+            "parentId": "1",
+            "title": "example.com",
+            "url": "https://example.com"
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 export default fakeBookmark;
